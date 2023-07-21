8d749ab4d4915901664002880914cef55ee384f8
uphilip1 <uphilip19@gmail.com>
2023-07-21 15:12:56 -0700

    100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- prints the largest prime factor
+ * of a number
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	long number = 612852475143;
+	int inc;
+
+	while (inc++ < number / 2)
+	{
+	if (number % inc == 0)
+	{
+		number /= 2;
+		continue;
+	}
